eb82e611e0a1279f6aeb90fcb02a5c505faeb3d4
Laurynas Gailius <laurynasgailius1@gmail.com>
2024-03-21 14:37:24 +0200

    Update updateProduct.js
    Changed response = await

--- controllers/updateProduct.js
+++ controllers/updateProduct.js
@@ -6,8 +6,9 @@ const Product = require('../models/productModel');
 async function updateProduct(id, updateDesc, data) {
   updateProductValidation(id, data);
 
-  const response = await Product.findByIdAndUpdate(id, {
+  const response = await Product.findByIdAndUpdate(id, data {
     description: updateDesc,
+      new: true,
   });
 
   return response;
